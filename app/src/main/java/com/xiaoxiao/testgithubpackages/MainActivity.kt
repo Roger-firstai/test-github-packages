f7accd9f24f00f18a7aa1fc30e5db0560d5c3322
Roger-firstai <luoshijie@firstai.ai>
2021-04-24 23:06:21 +0800

    提交项目

--- app/src/main/java/com/xiaoxiao/testgithubpackages/MainActivity.kt
+++ app/src/main/java/com/xiaoxiao/testgithubpackages/MainActivity.kt
@@ -2,10 +2,20 @@ package com.xiaoxiao.testgithubpackages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.xiaoxiao.common.Calculate
+import com.xiaoxiao.testgithubpackages.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mBinding.root)
+
+        mBinding.tvText.text = Calculate.add(20, 100).toString()
     }
 }
---
+++ common/src/main/java/com/xiaoxiao/common/Calculate.kt
@@ -0,0 +1,14 @@
+package com.xiaoxiao.common
+
+/**
+ * 作者：Roger`
+ * 时间：2021/04/23
+ * 说明：
+ */
+class Calculate {
+    companion object {
+        fun add(x: Int, y: Int): Int {
+            return x + y
+        }
+    }
+}
